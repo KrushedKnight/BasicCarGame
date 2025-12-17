d9e4423da8fcdc01e9a1b0cd2d6493883f90389e
Krishiv Manyam <krishivmanyam@gmail.com>
2025-12-16 16:54:56 -0800

    naive gearbox class implementation

---
+++ include/vehicle/Gearbox.h
@@ -0,0 +1,31 @@
+#ifndef SIMPLETRAFFICGAME_GEARBOX_H
+#define SIMPLETRAFFICGAME_GEARBOX_H
+
+#include <vector>
+
+class Engine;
+
+class Gearbox {
+private:
+    int selectedGear;
+    std::vector<double> gearRatios;
+    double finalDrive;
+    bool clutchEngaged;
+
+public:
+    Gearbox(const std::vector<double>& ratios, double finalDriveRatio);
+
+    double getFinalTorque(Engine* engine);
+
+    bool isClutchHeld() const;
+    void holdClutch();
+    void releaseClutch();
+
+    bool shiftUp();
+    bool shiftDown();
+
+    int getCurrentGear() const;
+    double getGearRatio();
+};
+
+#endif
---
+++ src/vehicle/Gearbox.cpp
@@ -0,0 +1,77 @@
+#include "vehicle/Gearbox.h"
+
+#include "vehicle/Engine.h"
+
+Gearbox::Gearbox(const std::vector<double>& ratios, double finalDriveRatio)
+{
+    this->gearRatios = ratios;
+    this->finalDrive = finalDriveRatio;
+    this->clutchEngaged = false;
+    this->selectedGear = -1;
+}
+
+bool Gearbox::isClutchHeld() const
+{
+    return true;
+}
+
+void Gearbox::holdClutch()
+{
+    clutchEngaged = true;
+}
+
+void Gearbox::releaseClutch()
+{
+    clutchEngaged = false;
+}
+
+double Gearbox::getFinalTorque(Engine* engine)
+{
+    double gearRatio = this->getGearRatio();
+    return engine->calculateTorque() * (gearRatio * this->finalDrive);
+}
+
+int Gearbox::getCurrentGear() const
+{
+    return selectedGear;
+}
+double Gearbox::getGearRatio()
+{
+    double gearRatio;
+    if (this->selectedGear == -1)
+    {
+        gearRatio = 0;
+    }
+    else if(this->selectedGear == -2)
+    {
+        gearRatio = -1.0 / this->gearRatios[0];
+    }
+    else
+    {
+        gearRatio = 1.0 / this->gearRatios[this->selectedGear];
+    }
+
+    return gearRatio;
+}
+
+bool Gearbox::shiftDown()
+{
+    if (selectedGear > -2)
+    {
+        selectedGear--;
+        return true;
+    }
+    return false;
+}
+
+bool Gearbox::shiftUp()
+{
+    if (selectedGear < this->gearRatios.size() - 1)
+    {
+        selectedGear++;
+        return true;
+    }
+    return false;
+}
+
+
